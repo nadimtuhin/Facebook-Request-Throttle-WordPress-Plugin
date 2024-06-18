5e07ff2a7e9b576e57a048aa40f9c1d5875d98e3
Tony Tony Woodworth <102385518+Anthony-eng21@users.noreply.github.com>
2024-06-18 09:23:16 -0600

    specify multiple user agents
    made a list for the useragents we want to throttle

--- facebook-request-throttle.php
+++ facebook-request-throttle.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Facebook Request Throttle
- * Description: Limits the request frequency from Facebook's web crawler.
- * Version: 1.0
+ * Plugin Name: User Agent Request Throttle
+ * Description: Limits the request frequency from specified user agents.
+ * Version: 1.1
  * Author: Nadim Tuhin
  * Author URI: https://nadimtuhin.com
  */
@@ -11,62 +11,81 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'We\'re sorry, but you can not directly access this file.' );
 }
 
+define('REQUEST_THROTTLE', 2.0); // Number of seconds permitted between each hit from specified user agents
 
-define('FACEBOOK_REQUEST_THROTTLE', 2.0); // Number of seconds permitted between each hit from facebookexternalhit
+// Array of user agents to throttle
+$user_agents_to_throttle = array(
+	'facebookexternalhit',
+	'Python/3.10 aiohttp/3.9.3',
+);
 
 /**
- * Check if the request is from Facebook's web crawler
+ * Check if the request is from specified user agents
  * 
+ * @param array $user_agents
  * @return bool
  */
-function nt_isRequestFromFacebook() {
-    return !empty($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'facebookexternalhit') !== false;
+function nt_isRequestFromUserAgents($user_agents) {
+	foreach ($user_agents as $agent) {
+		if (!empty($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], $agent) !== false) {
+			return true;
+		}
+	}
+	return false;
 }
 
-// ... [unchanged code above]
-
 /**
- * Get the last access time of Facebook's web crawler
+ * Get the last access time of the web crawler
  * 
+ * @param string $agent
  * @return float|null
  */
-function nt_getLastAccessTime() {
-  return get_transient('nt_facebook_last_access_time');
+function nt_getLastAccessTime($agent) {
+	return get_transient('nt_' . str_replace(' ', '_', $agent) . '_last_access_time');
 }
 
 /**
-* Set the last access time of Facebook's web crawler
-* 
-* @param float $microTime
-* @return bool
-*/
-function nt_setLastAccessTime($microTime) {
-  // Set the transient to last just slightly longer than the throttle time
-  return set_transient('nt_facebook_last_access_time', $microTime, FACEBOOK_REQUEST_THROTTLE + 1);
+ * Set the last access time of the web crawler
+ * 
+ * @param string $agent
+ * @param float $microTime
+ * @return bool
+ */
+function nt_setLastAccessTime($agent, $microTime) {
+	// Set the transient to last just slightly longer than the throttle time
+	return set_transient('nt_' . str_replace(' ', '_', $agent) . '_last_access_time', $microTime, REQUEST_THROTTLE + 1);
 }
 
-function nt_facebookRequestThrottle() {
-  $lastTime = nt_getLastAccessTime();
-  $microTime = microtime(TRUE);
+/**
+ * Throttle the request from the specified user agent
+ * 
+ * @param string $agent
+ */
+function nt_requestThrottle($agent) {
+	$lastTime = nt_getLastAccessTime($agent);
+	$microTime = microtime(TRUE);
 
-  // Separate the conditions to see which one is triggering
-  if (!$lastTime) {
-      error_log("No last access time found.");
-  } elseif ($microTime - $lastTime < FACEBOOK_REQUEST_THROTTLE) {
-      header($_SERVER["SERVER_PROTOCOL"] . ' 503 Service Temporarily Unavailable');
-      echo 'Service Temporarily Unavailable';
-      die;
-  }
-  
-  if (!nt_setLastAccessTime($microTime)) {
-      error_log("Failed to set last access time for Facebook web crawler.");
-      header($_SERVER["SERVER_PROTOCOL"] . ' 429 Too Many Requests');
-      echo 'Too Many Requests';
-      die;
-  }
+	// Separate the conditions to see which one is triggering
+	if (!$lastTime) {
+		error_log("No last access time found for $agent.");
+	} elseif ($microTime - $lastTime < REQUEST_THROTTLE) {
+		header($_SERVER["SERVER_PROTOCOL"] . ' 503 Service Temporarily Unavailable');
+		echo 'Service Temporarily Unavailable';
+		die;
+	}
+	
+	if (!nt_setLastAccessTime($agent, $microTime)) {
+		error_log("Failed to set last access time for $agent web crawler.");
+		header($_SERVER["SERVER_PROTOCOL"] . ' 429 Too Many Requests');
+		echo 'Too Many Requests';
+		die;
+	}
 }
 
 // Main logic
-if (nt_isRequestFromFacebook()) {
-  nt_facebookRequestThrottle();
+foreach ($user_agents_to_throttle as $agent) {
+	if (nt_isRequestFromUserAgents(array($agent))) {
+		nt_requestThrottle($agent);
+		break;
+	}
 }
